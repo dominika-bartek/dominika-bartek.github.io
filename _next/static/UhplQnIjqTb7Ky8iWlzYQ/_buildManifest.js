6afae0631a8336d0fc0b738cb0daf812cc5d95f6
Martyna <martyna.toloczko@flyingbisons.com>
2023-08-09 19:40:19 +0200

    Updates

---
+++ _next/static/UhplQnIjqTb7Ky8iWlzYQ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-61cf0e6ccb1a9ed111f1.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
